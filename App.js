527b8e7f345fa3089043f724ff8f1100cca7ae86
Harender Singh <89127398+Harender215@users.noreply.github.com>
2023-11-23 08:23:52 +0530

    Added Navbar to our app

--- App.js
+++ App.js
@@ -1,5 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-const heading = React.createElement("h1", {id:'heading', xyz: 'abc'}, "Hello from React");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+/* 
+Header
+    -logo
+    -navItem
+body
+    -search
+    -Restaurant Container
+        -Restaurant Card
+Footer
+    -copyright
+    -Links
+    -Address
+    -Contact
+*/
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className='logo' src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" alt="food_app_logo" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+        </div>
+    )
+    
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<AppLayout/>)
