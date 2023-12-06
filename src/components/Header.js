a0a2fe175231c1538191622b2a1bbfe9ae331017
Harender Singh <89127398+Harender215@users.noreply.github.com>
2023-12-06 21:40:36 +0530

    style header component using tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,20 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
-      <div className="header">
+      <div className="flex justify-between items-center bg-pink-100 shadow-sm mb-1">
         <div className="logo-container">
           <img
-            className="logo"
+            className="w-28"
             src={LOGO_URL}
             alt="food_app_logo"
           />
         </div>
         <div className="nav-items">
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About Us</Link></li>
-            <li><Link to="/contact">Contact Us</Link></li>
-            <li><Link>Cart</Link></li>
+          <ul className="flex p-4 m-4 items-center">
+            <li className="px-3"><Link to="/">Home</Link></li>
+            <li className="px-3"><Link to="/about">About Us</Link></li>
+            <li className="px-3"><Link to="/contact">Contact Us</Link></li>
+            <li className="px-3"><Link>Cart</Link></li>
           </ul>
         </div>
       </div>
